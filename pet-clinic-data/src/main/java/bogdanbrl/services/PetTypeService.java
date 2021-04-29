8e13bc76a69bd64acffb9d8cae3042a40054430d
Bogdan Brl <bogdan.brlx@gmail.com>
2021-04-29 12:53:33 +0300

    added PetTypeMapService Closes #7

---
+++ pet-clinic-data/src/main/java/bogdanbrl/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package bogdanbrl.services;
+
+import bogdanbrl.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
---
+++ pet-clinic-data/src/main/java/bogdanbrl/services/map/PetTypeMapService.java
@@ -0,0 +1,35 @@
+package bogdanbrl.services.map;
+
+import bogdanbrl.model.PetType;
+import bogdanbrl.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+}
