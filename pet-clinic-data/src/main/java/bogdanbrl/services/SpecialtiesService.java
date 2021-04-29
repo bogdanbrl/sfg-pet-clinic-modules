f628e9ad85507b7ff7bafba9d513256533a87084
Bogdan Brl <bogdan.brlx@gmail.com>
2021-04-29 14:20:43 +0300

    added specialties map impl Closes #9

---
+++ pet-clinic-data/src/main/java/bogdanbrl/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package bogdanbrl.services;
+
+import bogdanbrl.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long>{
+}
---
+++ pet-clinic-data/src/main/java/bogdanbrl/services/map/SpecialityMapService.java
@@ -0,0 +1,37 @@
+package bogdanbrl.services.map;
+
+import bogdanbrl.model.Specialty;
+import bogdanbrl.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return super.save(specialty);
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+}
