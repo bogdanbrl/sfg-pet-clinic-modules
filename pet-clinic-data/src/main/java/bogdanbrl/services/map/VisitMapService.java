575be6129e17d6d377d0de0389a4755313ef5542
Bogdan Brl <bogdan.brlx@gmail.com>
2021-04-30 10:54:52 +0300

    created spring data jpa visit service Closes #34

--- pet-clinic-data/src/main/java/bogdanbrl/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/bogdanbrl/services/map/VisitMapService.java
@@ -2,9 +2,11 @@ package bogdanbrl.services.map;
 
 import bogdanbrl.model.Visit;
 import bogdanbrl.services.VisitService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
 
     @Override
---
+++ pet-clinic-data/src/main/java/bogdanbrl/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,48 @@
+package bogdanbrl.services.springdatajpa;
+
+import bogdanbrl.model.Visit;
+import bogdanbrl.repositories.VisitRepository;
+import bogdanbrl.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return visitRepository.save(visit);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+}
